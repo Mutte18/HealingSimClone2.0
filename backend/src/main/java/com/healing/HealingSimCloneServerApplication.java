1f98cc1b22771d1b16d977652be691bb95d512ba
Mattias <mattias.vidhall@hotmail.com>
2021-05-08 19:53:41 +0200

    Create initial maven project

---
+++ backend/src/main/java/com/healing/HealingSimCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.healing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingSimCloneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingSimCloneServerApplication.class, args);
+    }
+
+}
